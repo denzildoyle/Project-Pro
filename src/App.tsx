9efdc1d01b7cdf84ee331665c07c44f237f251e8
Denzil Doyle <denzildoyle@gmail.com>
2025-08-13 12:28:09 -0400

    Add Player Report Page

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { StitchDesign } from "./screens/StitchDesign";
 import { PlayerAssessment } from "./screens/PlayerAssessment";
 import { AssessmentDetail } from "./screens/AssessmentDetail";
+import { PlayerReport } from "./screens/PlayerReport";
 
 export const App = (): JSX.Element => {
   return (
@@ -11,6 +12,7 @@ export const App = (): JSX.Element => {
         <Route path="/" element={<StitchDesign />} />
         <Route path="/player/:playerId/assessment" element={<PlayerAssessment />} />
         <Route path="/player/:playerId/assessment/:assessmentId" element={<AssessmentDetail />} />
+        <Route path="/player/:playerId/report" element={<PlayerReport />} />
       </Routes>
     </Router>
   );
