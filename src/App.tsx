142a4c311edf501bf47caf79cf5af9bdddbefff3
Denzil Doyle <denzildoyle@gmail.com>
2025-08-18 10:53:12 -0400

    Update title

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { StitchDesign } from "./screens/StitchDesign";
 import { PlayerAssessment } from "./screens/PlayerAssessment";
@@ -9,29 +11,47 @@ import { Matches } from "./screens/Matches";
 import { Drills } from "./screens/Drills";
 // import { NotFound } from "./screens/NotFound";
 
+const getPageTitle = (pathname: string): string => {
+	if (pathname.startsWith("/training")) return "Training | Project Pro";
+	if (pathname.startsWith("/matches")) return "Matches | Project Pro";
+	if (pathname.startsWith("/drills")) return "Drills | Project Pro";
+	if (pathname.startsWith("/register")) return "Register | Project Pro";
+	if (pathname.startsWith("/player")) return "Player | Project Pro";
+	return "Project Pro";
+};
+
+function AppRoutes() {
+	const location = useLocation();
+	console.log(location);
+	useEffect(() => {
+		document.title = getPageTitle(location.pathname);
+	}, [location.pathname]);
+
+	return (
+		<Routes>
+			<Route path="/" element={<StitchDesign />} />
+			<Route path="/register" element={<Registration />} />
+			<Route path="/training" element={<Training />} />
+			<Route path="/matches" element={<Matches />} />
+			<Route path="/drills" element={<Drills />} />
+			<Route
+				path="/player/:playerId/assessment"
+				element={<PlayerAssessment />}
+			/>
+			<Route
+				path="/player/:playerId/assessment/:assessmentId"
+				element={<AssessmentDetail />}
+			/>
+			<Route path="/player/:playerId/report" element={<PlayerReport />} />
+			{/* <Route path="*" element={<NotFound />} /> */}
+		</Routes>
+	);
+}
+
 export const App = (): JSX.Element => {
 	return (
 		<Router>
-			<Routes>
-				<Route path="/" element={<StitchDesign />} />
-				<Route path="/register" element={<Registration />} />
-				<Route path="/training" element={<Training />} />
-				<Route path="/matches" element={<Matches />} />
-				<Route path="/drills" element={<Drills />} />
-				<Route
-					path="/player/:playerId/assessment"
-					element={<PlayerAssessment />}
-				/>
-				<Route
-					path="/player/:playerId/assessment/:assessmentId"
-					element={<AssessmentDetail />}
-				/>
-				<Route
-					path="/player/:playerId/report"
-					element={<PlayerReport />}
-				/>
-			</Routes>
-			{/* <Route path="*" element={<NotFound />} /> */}
+			<AppRoutes />
 		</Router>
 	);
 };
