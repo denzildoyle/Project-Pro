e54ec4cc2a85ec5d5fd081d680fc7d0f206a703c
Denzil Doyle <denzildoyle@gmail.com>
2025-08-22 20:50:07 -0400

    add constants

---
+++ src/consts/positions.ts
@@ -0,0 +1,16 @@
+const positions = [
+	"Goalkeeper",
+	"Defender",
+	"Midfielder",
+	"Forward",
+	"Center Back",
+	"Full Back",
+	"Wing Back",
+	"Defensive Midfielder",
+	"Central Midfielder",
+	"Attacking Midfielder",
+	"Winger",
+	"Striker",
+];
+
+export default positions;
